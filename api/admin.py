42b9588bb7c679b23845f36c6c9158be7ae857e6
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:30 +0500

    Models registeration in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Category)
+admin.site.register(models.Task)
+admin.site.register(models.TaskAssigned)
+admin.site.register(models.Log)
