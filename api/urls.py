15bd3d19cd48f90b2274505dded1fa3020cd7ce7
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:33 +0500

    Add task_assigned endpoint to API

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("categories", views.categories, name="categories"),
     re_path(r"^task/(?P<pk>[0-9]+)$", views.task),
     path("task", views.task, name="task"),
+    re_path(r"^task_assigned/(?P<pk>[0-9]+)$", views.task_assigned),
+    path("task_assigned", views.task_assigned, name="task_assigned"),
 ]
