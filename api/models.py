5f7a2ae20aa8539bc8312b3a55f39e03847ffe89
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:28 +0500

    Created Task model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -11,3 +12,31 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=256)
+    priority = models.CharField(
+        max_length=10,
+        choices=[("Important", "Important"), ("Medium", "Medium"), ("Low", "Low")],
+        default="Medium",
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    progress = models.CharField(
+        max_length=10,
+        choices=[("Open", "Open"), ("In Progress", "In Progress"), ("Done", "Done")],
+        default="Open",
+    )
+    startDate = models.DateField(default=timezone.now)
+    endDate = models.DateField()
+    attachment = models.FileField(
+        null=True, blank=True, upload_to="static/attachments/"
+    )
+    description = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Tasks"
