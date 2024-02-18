6882bab8990cac184a6f11a05550eb3b00ffec4d
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:25 +0500

    Add UserAdmin class and register CustomUsers model in admin.py

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import Group
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    exclude = (
+        "first_name",
+        "last_name",
+        "groups",
+        "user_permissions",
+        "is_active",
+        "is_superuser",
+        "last_login",
+        "date_joined",
+        "username",
+    )
+    list_display = ("name", "email", "is_admin")
+    list_filter = (
+        "is_staff",
+        "timestamp",
+    )
+
+    def is_admin(self, obj):
+        pl = "✔️" if obj.is_staff else "❌"
+        return pl
+
+
+admin.site.register(models.CustomUsers, UserAdmin)
+
+
+admin.site.site_header = "Admin Panel"
+admin.site.site_title = "Admin Panel"
+admin.site.index_title = "Admin Panel"
+
+
+# Hide AUTHENTICATION AND AUTHORIZATION
+
+# from django.contrib.auth.models import User
+
+# admin.site.unregister(User)
+admin.site.unregister(Group)
