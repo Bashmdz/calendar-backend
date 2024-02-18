2deef830f7194db49f8a7473b14060b599ebbf8c
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:24 +0500

    Add user validation endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -14,6 +14,39 @@ from django.contrib.auth.hashers import check_password
 # Create your views here.
 
 
+@api_view(["POST"])
+@permission_classes([])
+@authentication_classes([])
+def validate(request):
+    if request.method == "POST":
+        # VALIDATE A USER
+        data = JSONParser().parse(request)
+        Users = models.CustomUsers.objects.all()
+        email = data["email"]
+        password = data["password"]
+        if email and password is not None:
+            count = Users.filter(email=email).count()
+            if count != 0:
+                data = Users.filter(email=email).values("password").first()
+                if (
+                    check_password(password, data["password"])
+                    or password == data["password"]
+                ):
+                    userData = Users.filter(email=email).first()
+                    SerializedData = serializers.ViewUserSerializer(
+                        userData, many=False
+                    )
+                    return JsonResponse(SerializedData.data, safe=False)
+                else:
+                    return JsonResponse({"message": "Incorrect password."}, safe=False)
+            else:
+                return JsonResponse(
+                    {"message": "No account found."},
+                    status=status.HTTP_200_OK,
+                )
+        return JsonResponse({"message": "empty"}, status=status.HTTP_200_OK)
+
+
 @api_view(["POST", "PUT", "GET", "DELETE"])
 @permission_classes([])
 @authentication_classes([])
