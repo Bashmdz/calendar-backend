cba5f0d2b72e746e38494dfafe3947ba16e7788c
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:29 +0500

    Task serializer created

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,17 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
         fields = "__all__"
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Task
+        fields = "__all__"
+
+
+class ViewTaskSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+
+    class Meta:
+        model = models.Task
+        fields = "__all__"
