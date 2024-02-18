7bfda6d36ffc3d21a00c75033af0717fb9c9a65e
Bashmdz <bashar.zheiman@web.de>
2024-02-18 17:49:32 +0500

    Add task endpoint to API URLs

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("categories", views.categories, name="categories"),
+    re_path(r"^task/(?P<pk>[0-9]+)$", views.task),
+    path("task", views.task, name="task"),
 ]
